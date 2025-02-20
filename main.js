ac6b0043c5113e18f0a3a2a85e32e5d79bc16975
skool04 <xxx0004k@gmail.com>
2025-02-21 06:56:43 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+var videoList = [
+  {
+    id: 1,
+    videoId: "https://www.youtube.com/embed/SmHsIi5gJNc?si=5ssWbX8_QfGyibi",
+    name: "Dẫu có lỗi lầm - binz",
+    vote: {
+      like: 1000,
+      dislike: 10,
+      isChecked: 0,
+    },
+    view: 8386
+  },
+  {
+    id: 2,
+    videoId: "https://www.youtube.com/embed/qusmHT5vNuw?si=nA3v3obAAzaKILAL",
+    name: "Đâu cần 1 bài ca tình yêu - TIÊN TIÊN",
+    vote: {
+      like: 200,
+      dislike: 50,
+      isChecked: 0,
+    },
+    view: 600
+  },
+  {
+    id: 3,
+    videoId: "https://www.youtube.com/embed/OBo1oOm8nlQ?si=VYqi5jfIXlJdDUuX",
+    name: "NGƯỜI BÌNH THƯỜNG - Đen Vâu",
+    vote: {
+      like: 200,
+      dislike: 50,
+      isChecked: 0,
+    },
+    view: 700
+  },
+  {
+    id: 4,
+    videoId: "https://www.youtube.com/embed/8sJl66EANuE?si=ZOM93YRxvLypjtNh",
+    name: "Yêu Thầm - Tlinh",
+    vote: {
+      like: 200,
+      dislike: 50,
+      isChecked: 0,
+    },
+    view: 800
+  },
+]
